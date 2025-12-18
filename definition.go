ef410daeee3fd167e04167388e845f96d2468101
Autumn <dazzleookami@outlook.com>
2025-12-18 01:15:41 -0800

    Move variables into define.go

--- definition.go
+++ definition.go
@@ -14,3 +14,34 @@ type SteamLibraryFolder struct {
 }
 
 var stop = make(chan bool)
+
+var help string = `Valid arguments: patchman.exe
+	help    		- Displays this help message
+	alias:
+		h
+		-h
+		--help
+		/h
+		/help
+	version 		- Displays patchman's version
+	alias:
+		v
+		-v
+		--version
+		/v
+		/version
+	status  		- Displays the current status
+	alias:
+		s
+		-s
+		--status
+		/s
+		/status
+
+If VTOL VR receives a new update and patches are yet to be marked as compatible, you could override the Build ID version by looking up a Build ID from https://github.com/bomkz/patchman-index and using it as follows:
+	patchman.exe <buildid>
+	patchman.exe 18407725`
+
+var patchmanversion string = "Patchman " + timestamp
+
+var timestamp string = "1748349190"

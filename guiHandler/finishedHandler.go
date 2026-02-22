652afccb459b00b71ce05d21d661afaef2dede4f
Autumn <dazzleookami@outlook.com>
2026-02-22 10:36:43 -0800

    Resized finished screen

--- guiHandler/finishedHandler.go
+++ guiHandler/finishedHandler.go
@@ -1,18 +1,20 @@
 package guiHandler
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 	"github.com/bomkz/patchman/global"
 )
 
 func handleFinish() {
-	finishText := widget.NewLabel("Patchman has finished installing your patch successfully.")
+	finishText := widget.NewLabel("Patchman has finished installing\nyour patch successfully.")
 	global.MainWindow.SetContent(container.NewVBox(
 		finishText,
 		container.NewHBox(
 			widget.NewButton("Finish", func() { global.ExitSuccess() }),
 		),
 	))
+	global.MainWindow.Resize(fyne.NewSize(100, 50))
 
 }
